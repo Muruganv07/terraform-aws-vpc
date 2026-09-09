4afdf06d699b14391935616726dd98d4e7bbfc1c
Muruganv07 <84227892+Muruganv07@users.noreply.github.com>
2026-09-09 16:29:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "terraform-github-vpc-testing-sep-07"
+    Name = var.vpc_name
   }
 }
